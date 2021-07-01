df1df6a025ba4809339ba552db515af00baf9afc
SohamUpadhyay08 <77781974+SohamUpadhyay08@users.noreply.github.com>
2021-07-01 16:28:14 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
-class MyButton extends Component{
-  render(){
-    return(
-       <Button color = "#325673" title = "submit"> </Button>
-    )
+
+class AnyButton extends Component {
+  displayAlert=()=>{
+    alert("it is a alert")
+  }
+  render() {
+    return (
+      <Button color = {this.props.color} title={this.props.title} onPress = {this.displayAlert}>
+      
+      </Button>
+    );
   }
 }
 
 export default class App extends Component {
   render() {
     return (
-      <View style = {{marginTop : 500}}>
-      <MyButton />
-      <MyButton />
-       <Text > name </Text>
-       </View>
+      <View style={{ marginTop: 200 }}>
+        <AnyButton color = "#568328" title = "2"/>
+        <AnyButton color = "#274692" title = "5"/>
+        <AnyButton color = "#292222" title = "8"/>
+        
+        <Text> check </Text>
+      </View>
     );
   }
 }
